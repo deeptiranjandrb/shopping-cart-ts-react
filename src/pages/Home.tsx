1813a034fefb04be4049dc5e06cd87936d938792
dbali00 <dbali00@bhnetwork.com>
2022-09-04 03:43:32 +0530

    text change in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 export function Home() {
     return <h1>
-        Welcome to the shopping cart site. Click on Store menu to see what awaits you.
+        Welcome to the shopping cart site. Click on the store menu to see what awaits you.
     </h1>
 }
